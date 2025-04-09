cd2f260c35f467552fd42426dd3cbb4d4e8ec17f
tiroxuki0 <citythree.11798@gmail.com>
2025-04-10 00:27:34 +0700

    change figma link

--- apps/minhhuy/src/contents/index/Header/HeaderCta.tsx
+++ apps/minhhuy/src/contents/index/Header/HeaderCta.tsx
@@ -36,7 +36,7 @@ function ButtonResume() {
     <a
       target="_blank"
       rel="noreferrer nofollow"
-      href="https://www.figma.com/community/file/1489868734848405862"
+      href="https://www.figma.com/community/file/1491851570835003912"
       className={clsx('button button--ghost px-2', 'md:button--big md:px-2')}
     >
       <DocumentIcon className={clsx('h-5 w-5')} />
